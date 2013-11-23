616c6b3ddcc5197c4a2eb63bd5351cfe77cb0c43
quentin <freezeeedos@gmail.com>
2013-11-23 15:50:05 +0100

    always flush iptables rules

--- rpi_wifi_ap.sh
+++ rpi_wifi_ap.sh
@@ -131,6 +131,9 @@ yes_no
 echo "net.ipv4.ip_forward=1" >> /etc/sysctl.conf
 echo 1 > /proc/sys/net/ipv4/ip_forward
 
+iptables -F
+iptables -t nat -F
+
 iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
 iptables -A FORWARD -i eth0 -o wlan0 -m state --state RELATED,ESTABLISHED -j ACCEPT
 iptables -A FORWARD -i wlan0 -o eth0 -j ACCEPT
